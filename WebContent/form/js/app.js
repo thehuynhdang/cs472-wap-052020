3150a38bac22d8c94f00153cd0b63d476e76c77e
The Huynh Dang <dangthehuynh@gmail.com>
2020-04-26 16:17:31 -0500

    lab6

---
+++ WebContent/form/js/app.js
@@ -0,0 +1,41 @@
+function logLoginConsole() {
+    var username = document.getElementById("uname");
+    console.log("Username=" + username.value);
+
+    var pwd = document.getElementById("pwd");
+    console.log("Password=" + pwd.value);
+}
+
+function showNewProductInfo() {
+    var productNbr = document.getElementById("txtProductNbr").value;
+    document.getElementById("showProductNbr").innerText = productNbr;
+
+    //txtQuantity
+    var quantity = document.getElementById("txtQuantity").value;
+    document.getElementById("showQuantity").innerText = quantity;
+
+    //txtName
+    var name = document.getElementById("txtName").value;
+    document.getElementById("showName").innerText = name;
+
+    //txtSupplier
+    var supplier = document.getElementById("txtSupplier").value;
+    document.getElementById("showSupplier").innerText = supplier;
+
+    //txtPrice
+    var price = document.getElementById("txtPrice").value;
+    document.getElementById("showPrice").innerText = price;
+
+    //txtSuppliedDate
+    var suppliedDate = document.getElementById("txtSuppliedDate").value;
+    document.getElementById("showSuppliedDate").innerText = suppliedDate;
+}
+
+function clock() {
+    setInterval(function() {
+            var now = new Date();
+            const date = now.toLocaleDateString();
+            const time = now.toLocaleTimeString();
+            document.getElementById("showClock").innerText = `${date} ${time}`;
+        }, 1000);
+}
