0bd9ac302f9605625c536ea499767f3981bd87bb
The Huynh Dang <dangthehuynh@gmail.com>
2020-04-30 22:22:08 -0500

    lab09

--- WebContent/form/js/lab09-jquery.js
+++ WebContent/form/js/lab09-jquery.js
@@ -4,5 +4,44 @@
  */
 
 $(document).ready(function() {
-	alert('jQuery enabled.');
+
+	$("#frmLogin").submit(function(event) {
+		event.preventDefault();
+		
+		const username = $("#uname");
+	    console.log("Username=" + username.val());
+
+	    const pwd = $("#pwd");
+	    console.log("Password=" + pwd.val());
+	});
+	
+	
+	$("#frmNewProduct").submit(function(event) {
+		event.preventDefault();
+		
+		const productNbr = $("#txtProductNbr").val();
+	    $("#showProductNbr").text(productNbr);
+
+	    //txtQuantity
+	    const quantity = $("#txtQuantity").val();
+	    $("#showQuantity").text(quantity);
+
+	    //txtName
+	    const name = $("#txtName").val();
+	    $("#showName").text(name);
+
+	    //txtSupplier
+	    const supplier = $("#txtSupplier").val();
+	    $("#showSupplier").text(supplier);
+
+	    //txtPrice
+	    const price = $("#txtPrice").val();
+	    $("#showPrice").text(price);
+
+	    //txtSuppliedDate
+	    const suppliedDate = $("#txtSuppliedDate").val();
+	    $("#showSuppliedDate").text(suppliedDate);
+	    
+	    $("#showInfoModel").show();
+	});
 });
