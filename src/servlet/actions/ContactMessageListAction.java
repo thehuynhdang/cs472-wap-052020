6a61ed84c2cde9639eb5688fdb175d6dc87531d4
The Huynh Dang <dangthehuynh@gmail.com>
2020-05-10 17:26:07 -0500

    clean

--- src/servlet/actions/ContactMessageListAction.java
+++ src/servlet/actions/ContactMessageListAction.java
@@ -3,12 +3,9 @@ package servlet.actions;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import model.ContactMessage;
 import service.ContactMessageServiceImpl;
@@ -24,30 +21,6 @@ public class ContactMessageListAction extends BaseAction<BaseValidationForm>  {
 	private static final long serialVersionUID = 1L;
 	
 	private IContactMessageService contactMessageService = new ContactMessageServiceImpl();
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getAttribute("contactMessages") == null) {
-        	 List<ContactMessage> contactMessages = contactMessageService.loadContactMessages();
-        	 request.setAttribute("contactMessages", contactMessages);
-        }
-
-		request.getRequestDispatcher("/WEB-INF/pages/contact-messages-list.jsp").forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-	
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init();
-    }
 
 	@Override
 	public String execute(BaseValidationForm validationForm) throws ServletException, IOException {
